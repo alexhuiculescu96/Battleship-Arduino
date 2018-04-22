0977d0c888ec669e390b277bd11d8f8e84832706
alexhuiculescu96 <alex.huiculescu@student.upt.ro>
2018-04-22 18:12:53 +0300

    Apply align + made ButtonListener able to listen for key presses and return the last pressed button

--- battleship2/battleship2.ino
+++ battleship2/battleship2.ino
@@ -1,10 +1,10 @@
 //Set this depending on environment
 #define arduino true
 #if !arduino
-    #include <iostream>
-    #include "LedControl.h"
+#include <iostream>
+#include "LedControl.h"
 #else
-    #include <LedControl.h>
+#include <LedControl.h>
 #endif
 #define columnsNumber 8
 #define rowsNumber 8
@@ -14,8 +14,8 @@
 using namespace std;
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+  cout << "Hello world!" << endl;
+  return 0;
 }
 #endif
 
@@ -25,18 +25,97 @@ enum LedState
   LED_OFF = 0,
   LED_INTERMITENT = 2
 };
+static const int row1 = 2;
+static const int row2 = 3;
+static const int row3 = 4;
+static const int row4 = 5;
+static const int colA = 6;
+static const int colB = 7;
+static const int colC = 8;
+static const int colD = 9;
 const char numberOfShips = 5;
 char ships[] = {4, 3, 3, 2, 2};
 char columnNumber = 8;
-class ButtonListener {
-  char pressedButton;
-  public:
-  bool checkPressedButton(){
-    //TODO get pressed button and save it in pressedButton
-    return false;
+class ButtonListener
+{
+  char pressedButtonValue = -1;
+  char noKey = 0;
+  boolean readKey = false;
+  boolean buttonPressed = false;
+
+public:
+  bool listen()
+  {
+    if (this->noKey == 16)
+    {// no keys were pressed
+      this->readKey = true; // keyboard is ready to accept a new keypress
+    }
+    this->noKey = 0;
+    int rowCounter = 0;
+    int columnCounter = 0; // column counter
+    for (rowCounter = row1; rowCounter < (row4 + 1); rowCounter++)
+    {
+      scanRow(rowCounter); // switch on one row at a time
+      for (columnCounter = colA; columnCounter < colD + 1; columnCounter++)
+      {
+        if (readColumn(columnCounter)) // read the switch pressed
+        {//the button has been pressed
+          this->pressedButtonValue = (rowCounter - row1) + 4 * (columnCounter - colA);
+        }
+        else
+        {
+          this->noKey++; // counter for number of empty columns
+        }
+      }
+    }
+    if (this->readKey == true && this->noKey == 15)
+    {
+      // a key has been pressed
+      Serial.println(this->pressedButtonValue); // used for debug
+      this->buttonPressed = true;
+      this->readKey = false; // rest the flag
+    }
   }
-  char getPressedButton(){
-    return this->pressedButton;
+
+  boolean buttonHasBeenPressed() {
+    if(this->buttonPressed) {
+      this->buttonPressed = false;
+      return true;
+    } else return false;
+  }
+
+  char getPressedButton()
+  {
+    return this->pressedButtonValue;
+  }
+
+  void setup()
+  {
+    Serial.begin(9600);
+    pinMode(row1, OUTPUT);
+    pinMode(row2, OUTPUT);
+    pinMode(row3, OUTPUT);
+    pinMode(row4, OUTPUT);
+    pinMode(colA, INPUT_PULLUP);
+    pinMode(colB, INPUT_PULLUP);
+    pinMode(colC, INPUT_PULLUP);
+    pinMode(colD, INPUT_PULLUP);
+  }
+
+  bool readColumn(int columnNumber)
+  {
+    //digitalRead returns 0 when button was pressed
+    int foundColumn = digitalRead(columnNumber) ? false : true;
+    return foundColumn;
+  }
+
+  void scanRow(int rowNumber)
+  {
+    for (int j = row1; j < (row4 + 1); j++)
+    {
+      digitalWrite(j, HIGH);
+    }
+    digitalWrite(rowNumber, LOW); // switch on one row
   }
 };
 
@@ -125,9 +204,10 @@ public:
   char oldRow;
   char oldColumn;
   bool cursorHasMoved;
-  Cursor() {
-      this->row = 0;
-      this->column = 0;
+  Cursor()
+  {
+    this->row = 0;
+    this->column = 0;
   }
   Cursor(char rowN, char colN)
   {
@@ -267,27 +347,30 @@ public:
   Cursor cursor;
   char currentPageNumber;
   bool currentPageHasChanged;
-  bool beingAttacked(char rowNumber, char columnNumber) {
+  bool beingAttacked(char rowNumber, char columnNumber)
+  {
     //TODO instead of returning <bool>, return a custom struct
     //with another info, like: a ship was destroyed, and ship's coords
     this->allEnemyShots[rowNumber][columnNumber] = LED_ON;
-    if(this->myShipsMatrix[rowsNumber][columnsNumber] == LED_ON) {
-    this->allEnemyHits[rowNumber][columnNumber] = LED_ON;
-    return true;
+    if (this->myShipsMatrix[rowsNumber][columnsNumber] == LED_ON)
+    {
+      this->allEnemyHits[rowNumber][columnNumber] = LED_ON;
+      return true;
     }
     return false;
   }
   //Returns if a ship was completly destroied (UNIMPLEMENTED)
-  bool hasJustAttacked(char rowNumber, char columnNumber, bool isHit) {
+  bool hasJustAttacked(char rowNumber, char columnNumber, bool isHit)
+  {
     //TODO save ships coordonates in generateShips method and check if this shot destroyed
     //a ship or just damaged it. And change <isHit> arg with a custom struct
-    this->allMyShots[rowNumber][columnNumber]= LED_ON;
+    this->allMyShots[rowNumber][columnNumber] = LED_ON;
     this->allMyHits[rowNumber][columnNumber] = LED_ON;
     return false;
   }
-  Player(GameHelper gameHelper) :  myShipsMatrix(create2DArray(8,8)), allMyShots(create2DArray(8,8)),
-    allMyHits(create2DArray(8,8)), allEnemyShots(create2DArray(8,8)), allEnemyHits(create2DArray(8,8)),
-    cursor(0,0)
+  Player(GameHelper gameHelper) : myShipsMatrix(create2DArray(8, 8)), allMyShots(create2DArray(8, 8)),
+                                  allMyHits(create2DArray(8, 8)), allEnemyShots(create2DArray(8, 8)), allEnemyHits(create2DArray(8, 8)),
+                                  cursor(0, 0)
   {
     this->currentPageNumber = 0;
     gameHelper.initializeMatrix(this->myShipsMatrix);
@@ -298,49 +381,60 @@ public:
     gameHelper.initializeMatrix(this->allEnemyHits);
     //this->cursor = new Cursor(0, 0);
   }
-  char** create2DArray(unsigned height, unsigned width)
+  char **create2DArray(unsigned height, unsigned width)
+  {
+    char **array2D = 0;
+    array2D = new char *[height];
+
+    for (char h = 0; h < height; h++)
     {
-      char** array2D = 0;
-      array2D = new char*[height];
+      array2D[h] = new char[width];
 
-      for (char h = 0; h < height; h++)
+      for (char w = 0; w < width; w++)
       {
-            array2D[h] = new char[width];
-
-            for (char w = 0; w < width; w++)
-            {
-                  // fill in some initial values
-                  // (filling in zeros would be more logic, but this is just for the example)
-                  array2D[h][w] = w + width * h;
-            }
+        // fill in some initial values
+        // (filling in zeros would be more logic, but this is just for the example)
+        array2D[h][w] = w + width * h;
       }
-
-      return array2D;
     }
 
-char** getPage(char pageNumber) {
-    pageNumber = pageNumber%5;
-    switch(pageNumber) {
-      case 0: return this->myShipsMatrix;
-      case 1: return this->allMyShots;
-      case 2: return this->allMyHits;
-      case 3: return this->allEnemyShots;
-      case 4: return this->allEnemyHits;
+    return array2D;
+  }
+
+  char **getPage(char pageNumber)
+  {
+    pageNumber = pageNumber % 5;
+    switch (pageNumber)
+    {
+    case 0:
+      return this->myShipsMatrix;
+    case 1:
+      return this->allMyShots;
+    case 2:
+      return this->allMyHits;
+    case 3:
+      return this->allEnemyShots;
+    case 4:
+      return this->allEnemyHits;
     }
     return NULL;
   }
-  char** getCurrentPage() {
+  char **getCurrentPage()
+  {
     return this->getPage(currentPageNumber);
   }
-  void switchPageLeft() {
+  void switchPageLeft()
+  {
     this->currentPageNumber = (this->currentPageNumber - 1) % 5;
     this->currentPageHasChanged = true;
   }
-  void switchPageRight() {
+  void switchPageRight()
+  {
     this->currentPageNumber = (this->currentPageNumber + 1) % 5;
     this->currentPageHasChanged = true;
   }
-  void changePage(char newPageNumber) {
+  void changePage(char newPageNumber)
+  {
     this->currentPageNumber = newPageNumber % 5;
     this->currentPageHasChanged = true;
   }
@@ -355,14 +449,14 @@ char** getPage(char pageNumber) {
 LedControl lc = LedControl(12, 10, 11, 2);
 ButtonListener listener = ButtonListener();
 GameHelper gameHelper = GameHelper();
-Player players[2]={Player(gameHelper), Player(gameHelper)};
+Player players[2] = {Player(gameHelper), Player(gameHelper)};
 Cursor currentCursor;
 char currentPlayer = 0;
 char **currentActiveMatrix;
 char loserPlayer = -1;
 bool gameStarted = false;
 bool isCursorVisible = false;
-int winnerCount=0;
+int winnerCount = 0;
 char count;
 
 enum action
@@ -381,21 +475,21 @@ void startGame(char firstPlayer);
 void buttonPressed(char pressedButton);
 void displayWinnerMessage(char winner);
 void displayLoserMessage(char loser);
-void displayStaticMatrix(char **matrixToDisplay,char deviceId);
+void displayStaticMatrix(char **matrixToDisplay, char deviceId);
 bool isCursorActiveOnPage(char pageNumber);
-void renderIntermitents(char **matrixToDisplay,char deviceId);
-void displayDynamicMatrix(char **matrixToDisplay, char deviceId,bool value);
+void renderIntermitents(char **matrixToDisplay, char deviceId);
+void displayDynamicMatrix(char **matrixToDisplay, char deviceId, bool value);
 void makeHitAnimation(char deviceId, char rowNumber, char columnNumber);
 void changeCurrentPlayer(char newPlayerNumber);
-void displayStaticMatrix(char **matrixToDisplay,char deviceId);
+void displayStaticMatrix(char **matrixToDisplay, char deviceId);
 void setup()
 {
-    //ON ARDUINO: change "::" with '.'
-  #if arduino
-    Serial.begin(9600);
-  #else
-    Serial::begin(9600);
-  #endif
+//ON ARDUINO: change "::" with '.'
+#if arduino
+  Serial.begin(9600);
+#else
+  Serial::begin(9600);
+#endif
   randomSeed(analogRead(0));
   /*
    The MAX72XX is in power-saving mode on startup,
@@ -409,94 +503,117 @@ void setup()
   /* and clear the display */
   lc.clearDisplay(0);
   lc.clearDisplay(1);
+  listener.setup();
   startGame(-1);
 }
 void loop()
 {
   delay(10);
-  if (listener.checkPressedButton())
+  listener.listen();
+  if (listener.buttonHasBeenPressed())
   {
     buttonPressed(listener.getPressedButton());
   }
   //The following check could be used to delay the game start until one player presses a button
   //and to support successive games
-  if(!gameStarted) {
-    if(loserPlayer > -1) {
-      displayWinnerMessage((loserPlayer+1)%2);
+  if (!gameStarted)
+  {
+    if (loserPlayer > -1)
+    {
+      displayWinnerMessage((loserPlayer + 1) % 2);
       displayLoserMessage(loserPlayer);
-      if(winnerCount >1000)
-        startGame((loserPlayer+1)%2);
-    } else return;
+      if (winnerCount > 1000)
+        startGame((loserPlayer + 1) % 2);
+    }
+    else
+      return;
   }
-  if(players[currentPlayer].currentPageHasChanged) {
+  if (players[currentPlayer].currentPageHasChanged)
+  {
     players[currentPlayer].currentPageHasChanged = false;
     currentActiveMatrix = players[currentPlayer].getCurrentPage();
     displayStaticMatrix(currentActiveMatrix, currentPlayer);
     isCursorVisible = isCursorActiveOnPage(players[currentPlayer].currentPageNumber);
   }
-  if(currentCursor.cursorHasMoved) {
+  if (currentCursor.cursorHasMoved)
+  {
     //After the cursor has moved, restore the previous led to it's initial state
     //Otherwise, bacause cursor is intermitent, it's state would be lost
     currentCursor.cursorHasMoved = false;
     char r = currentCursor.oldRow;
     char c = currentCursor.oldColumn;
-    if(currentActiveMatrix[r][c] == LED_ON ) lc.setLed(currentPlayer, r, c, true);
-    if(currentActiveMatrix[r][c] == LED_OFF) lc.setLed(currentPlayer, r, c, false);
+    if (currentActiveMatrix[r][c] == LED_ON)
+      lc.setLed(currentPlayer, r, c, true);
+    if (currentActiveMatrix[r][c] == LED_OFF)
+      lc.setLed(currentPlayer, r, c, false);
     //We don't need to set a led with state euqal to LED_INTERMITENT, since it will be activated/deactivatet
     //in renderIntermitents() method
   }
-  renderIntermitents(currentActiveMatrix,currentPlayer);
+  renderIntermitents(currentActiveMatrix, currentPlayer);
 }
 
-void displayWinnerMessage(char winner) {
+void displayWinnerMessage(char winner)
+{
   winnerCount++;
-  for(int i=0;i<8;i++)
-  lc.setRow(winner,i,true);
+  for (int i = 0; i < 8; i++)
+    lc.setRow(winner, i, true);
 }
-void displayLoserMessage(char loser) {
-  for(int i=0;i<8;i++)
-  lc.setRow(loser,i,false);
+void displayLoserMessage(char loser)
+{
+  for (int i = 0; i < 8; i++)
+    lc.setRow(loser, i, false);
 }
-void renderIntermitents(char **matrixToDisplay,char deviceId)
+void renderIntermitents(char **matrixToDisplay, char deviceId)
 {
   count++;
-  if(count == 50) {
-    displayDynamicMatrix( matrixToDisplay, deviceId, true);
-  } else if(count == 100) {
+  if (count == 50)
+  {
+    displayDynamicMatrix(matrixToDisplay, deviceId, true);
+  }
+  else if (count == 100)
+  {
     count = 0;
-    displayDynamicMatrix( matrixToDisplay, deviceId, false);
+    displayDynamicMatrix(matrixToDisplay, deviceId, false);
   }
 }
-void displayDynamicMatrix(char **matrixToDisplay, char deviceId,bool value) {
-  char i,j;
-  for(i=0;i<8;i++) {
-    for(j=0;j<8;j++)
-    {
-      if(matrixToDisplay[i][j] == LED_INTERMITENT) lc.setLed(deviceId, i, j, value);
-    }
-  }
-  //display cursor
-  if(isCursorVisible) lc.setLed(deviceId, currentCursor.row,currentCursor.column, value);
-}
-
-void displayStaticMatrix(char **matrixToDisplay,char deviceId)
+void displayDynamicMatrix(char **matrixToDisplay, char deviceId, bool value)
 {
   char i, j;
   for (i = 0; i < 8; i++)
   {
     for (j = 0; j < 8; j++)
     {
-      if(matrixToDisplay[i][j] == LED_ON) lc.setLed(deviceId, i, j, true);
-      if(matrixToDisplay[i][j] == LED_OFF) lc.setLed(deviceId,i,j, false);
+      if (matrixToDisplay[i][j] == LED_INTERMITENT)
+        lc.setLed(deviceId, i, j, value);
     }
   }
-
+  //display cursor
+  if (isCursorVisible)
+    lc.setLed(deviceId, currentCursor.row, currentCursor.column, value);
 }
-bool isCursorActiveOnPage(char pageNumber) {
-  if(pageNumber == 1) return true;
+
+void displayStaticMatrix(char **matrixToDisplay, char deviceId)
+{
+  char i, j;
+  for (i = 0; i < 8; i++)
+  {
+    for (j = 0; j < 8; j++)
+    {
+      if (matrixToDisplay[i][j] == LED_ON)
+        lc.setLed(deviceId, i, j, true);
+      if (matrixToDisplay[i][j] == LED_OFF)
+        lc.setLed(deviceId, i, j, false);
+    }
+  }
+}
+bool isCursorActiveOnPage(char pageNumber)
+{
+  if (pageNumber == 1)
+    return true;
   return false;
 }
-void playerHasRetreated(char loser) {
+void playerHasRetreated(char loser)
+{
   //TODO display something to show that the game has ended
   gameStarted = false;
   loserPlayer = loser;
@@ -505,13 +622,15 @@ void startGame(char firstPlayer)
 {
   players[0] = Player(gameHelper);
   players[1] = Player(gameHelper);
-  if(firstPlayer != 0 && firstPlayer != 1) currentPlayer = random() % 2;
-  else currentPlayer = firstPlayer;
+  if (firstPlayer != 0 && firstPlayer != 1)
+    currentPlayer = random() % 2;
+  else
+    currentPlayer = firstPlayer;
   currentCursor = players[currentPlayer].cursor;
   isCursorVisible = false;
   //Show ships to both players
-  displayStaticMatrix(players[0].myShipsMatrix,0);
-  displayStaticMatrix(players[1].myShipsMatrix,1);
+  displayStaticMatrix(players[0].myShipsMatrix, 0);
+  displayStaticMatrix(players[1].myShipsMatrix, 1);
   count = 0;
   winnerCount = 0;
   loserPlayer = -1;
@@ -521,19 +640,23 @@ void startGame(char firstPlayer)
 }
 void moveCursorLeft()
 {
-  if(isCursorVisible) currentCursor.moveLeft(1);
+  if (isCursorVisible)
+    currentCursor.moveLeft(1);
 }
 void moveCursorRight()
 {
-  if(isCursorVisible) currentCursor.moveRight(1);
+  if (isCursorVisible)
+    currentCursor.moveRight(1);
 }
 void moveCursorUp()
 {
-  if(isCursorVisible) currentCursor.moveUp(1);
+  if (isCursorVisible)
+    currentCursor.moveUp(1);
 }
 void moveCursorDown()
 {
-  if(isCursorVisible) currentCursor.moveDown(1);
+  if (isCursorVisible)
+    currentCursor.moveDown(1);
 }
 void switchPageLeft()
 {
@@ -549,43 +672,46 @@ void retreat()
 }
 void enter()
 {
-  if(!isCursorVisible) return;
+  if (!isCursorVisible)
+    return;
   char rowNumber = currentCursor.row;
   char columnNumber = currentCursor.column;
-  Player enemy = players[(currentPlayer+1)%2];
+  Player enemy = players[(currentPlayer + 1) % 2];
   bool isAHit = enemy.beingAttacked(rowNumber, columnNumber);
-  if(isAHit) {
+  if (isAHit)
+  {
     //There is a hit
-    makeHitAnimation(currentPlayer,rowNumber, columnNumber);
+    makeHitAnimation(currentPlayer, rowNumber, columnNumber);
     players[currentPlayer].hasJustAttacked(rowNumber, columnNumber, true);
     //Since it's a hit, we don't change the player
     return;
   }
   players[currentPlayer].hasJustAttacked(rowNumber, columnNumber, false);
   //Change the player
-  changeCurrentPlayer((currentPlayer+1)%2);
-
+  changeCurrentPlayer((currentPlayer + 1) % 2);
 }
 
-void changeCurrentPlayer(char newPlayerNumber) {
+void changeCurrentPlayer(char newPlayerNumber)
+{
   currentPlayer = newPlayerNumber;
   currentCursor = players[currentPlayer].cursor;
   currentActiveMatrix = players[currentPlayer].getCurrentPage();
 }
 
-void makeHitAnimation(char deviceId,char rowNumber,char columnNumber) {
+void makeHitAnimation(char deviceId, char rowNumber, char columnNumber)
+{
   bool value = true;
   char i;
-  for(i=0;i<9;i++)
+  for (i = 0; i < 9; i++)
   {
-    lc.setLed(deviceId,rowNumber,columnNumber,value);
+    lc.setLed(deviceId, rowNumber, columnNumber, value);
     value = !value;
     delay(300);
   }
 }
 void buttonPressed(char pressedBttn)
 {
-    int pressedButton = (int) pressedBttn;
+  int pressedButton = (int)pressedBttn;
   switch (pressedButton)
   {
   case moveCursorLeftAction:
@@ -634,5 +760,3 @@ void buttonPressed(char pressedBttn)
   }
   }
 }
-
-
